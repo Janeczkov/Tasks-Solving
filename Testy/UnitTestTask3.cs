29bd829cab093dda73f50c7f57bb0e850894e2f8
Janeczkov <lystekk@gmail.com>
2023-03-14 23:32:35 +0100

    Cleanup - preparing for task 3

--- Testy/UnitTestTask3.cs
+++ Testy/UnitTestTask3.cs
@@ -9,22 +9,7 @@ namespace TaskTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("jc", Tasks.Task2.StringChallenge("abjchba"));
-        }
-        [TestMethod]
-        public void TestMethod2()
-        {
-            Assert.AreEqual("not possible", Tasks.Task2.StringChallenge("mmop"));
-        }
-        [TestMethod]
-        public void TestMethod3()
-        {
-            Assert.AreEqual("k", Tasks.Task2.StringChallenge("kjjjhjjj"));
-        }
-        [TestMethod]
-        public void TestMethod4()
-        {
-            Assert.AreEqual("palindrome", Tasks.Task2.StringChallenge("abba"));
+            
         }
     }
 }
--- TestyDoCV/Task3.cs
+++ TestyDoCV/Task3.cs
@@ -8,57 +8,11 @@ namespace Tasks
 {
     public class Task3
     {
-        public static string StringChallenge(string str)
+        public static string ArrayChallenge(int[] arr)
         {
             try
             {
-                char[] strArr = str.ToCharArray();
 
-                int j = strArr.Length-1;
-                bool skippedLetter = false;
-                string skippedLetters = "";
-                //int indexOfLastPalindromeMatch = 0;
-                int numberOfSkippedLetters = 0;
-                bool loopBreaked = false;
-                for (int i=0; i < j; i++)
-                {
-                    if (strArr[i] != strArr[j])
-                    {
-                        if (numberOfSkippedLetters >= 2)
-                        {
-                            //i = Math.Max(indexOfLastPalindromeMatch - 1, 0);
-                            loopBreaked = true;
-                            break;
-                        }
-                        else
-                        {
-                            if (numberOfSkippedLetters == 0)
-                            {
-                                //indexOfLastPalindromeMatch = i;
-                            }
-                            numberOfSkippedLetters++;
-                            skippedLetter = true;
-                            skippedLetters += strArr[i].ToString();
-                        }
-                    }
-                    if (!skippedLetter)
-                    {
-                        j--;// strArr.Length - 1 - 1;
-                    }
-                    skippedLetter = false;
-                }
-                if (loopBreaked)
-                {
-                    return "not possible";
-                }
-                else
-                {
-                    if (skippedLetters == "")
-                    {
-                        return "palindrome";
-                    }
-                    return skippedLetters;
-                }
             }
             catch (Exception ex)
             {
