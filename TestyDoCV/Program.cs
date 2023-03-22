bb6eb625f5c847e5de369bdabd7e6b5d06a9f36f
Janeczkov <lystekk@gmail.com>
2023-03-23 00:04:38 +0100

    logic fixing

--- TestyDoCV/Program.cs
+++ TestyDoCV/Program.cs
@@ -11,7 +11,8 @@ namespace Tasks
         static void Main(string[] args)
         {
             //Console.WriteLine(Task1.StringChallenge("aabbcde"));
-            Console.WriteLine(Task2.StringChallenge("kjjjhjjj"));
+            //Console.WriteLine(Task2.StringChallenge("kjjjhjjj"));
+            Console.WriteLine(Task3.ArrayChallenge(new int[] { 12, 2, 6, 7, 11 }));
             Console.ReadKey();
         }
 
--- TestyDoCV/Task3.cs
+++ TestyDoCV/Task3.cs
@@ -12,6 +12,49 @@ namespace Tasks
         {
             try
             {
+                if (arr.Length <= 2)
+                {
+                    return "-1";
+                }
+                else
+                {
+                    int slotsCount = arr[0];
+                    List<int> slotsOccupied = arr.Skip(1).ToList();
+                    List<Tuple<int, int>> validPairs = new List<Tuple<int, int>>();
+                    List<int> slotsFreeToTake = new List<int>();
+
+                    for (int i = 1; i <= slotsCount; i++)
+                    {
+                        if (!slotsOccupied.Contains(i))
+                            slotsFreeToTake.Add(i);
+                    }
+
+                    //additional validations:
+                    //slots count, slots occupied only positive integers
+                    foreach (int i in slotsFreeToTake) //iterate through free slots
+                    {
+                        for (int j = i + 1; j <= i + 2; j++) //iterate through range of specific slot, for 3 it will be 1,2,3,4,5
+                        {
+                            if (j != i && j > 0 && j <= slotsCount && slotsFreeToTake.Contains(j)) //do not compare range iterator with same number
+                            {
+                                if (i % 2 == 0 && i+1 != j) //if even (2,4...) then do not compare even with even+1 (2 and 3) because its cross comparision (check image)
+                                {
+                                    validPairs.Add(new Tuple<int, int>(i, j));
+                                }
+                                else if (i % 2 == 1)
+                                {
+                                    validPairs.Add(new Tuple<int, int>(i, j));
+                                }
+                            }
+                        }
+                    }
+                    var returnString = string.Join(" | ", validPairs.Select(z => z.Item1 + "," + z.Item2));
+                    Console.WriteLine(returnString);
+
+                }
+
+                return "0";
+
 
             }
             catch (Exception ex)
